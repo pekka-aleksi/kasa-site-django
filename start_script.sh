3989da906d22b31eef44a6c02f7667f5eb926878
Pekka Aleksi Kasa <github@kasa.site>
2024-03-16 16:57:39 +0200

    Changes to services.

--- start_script.sh
+++ start_script.sh
@@ -8,4 +8,5 @@ nginx
 python manage.py makemigrations kasa_site_django
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py createsuperuser --noinput --username ${DJANGO_SUPERUSER_USERNAME} --email ${DJANGO_SUPERUSER_EMAIL}
 python manage.py runserver 0.0.0.0:8000
